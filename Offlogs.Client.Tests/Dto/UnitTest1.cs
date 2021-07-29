82c25d92780efc4b3cc37a921d50ef5a885060a8
Heorhii Shyra <hshy@isd.dp.ua>
2021-07-29 17:01:30 +0300

    #1 Added first tests

---
+++ Offlogs.Client.Tests/Dto/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Offlogs.Client.Tests.Dto
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
