0d77f6692473b5d3257797c8b552f8e621c504b9
Heorhii Shyra <hshy@isd.dp.ua>
2021-07-30 13:58:08 +0300

    #6 Added base http tests

---
+++ Offlogs.Client.Tests/BaseTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Offlogs.Client.Tests
+{
+    public class BaseTest : IDisposable
+    {
+        protected static IConfiguration Configuration;
+
+        protected string ApiToken
+        {
+            get => Configuration.GetValue<string>("OffLogs:TestApiToken");
+        }
+
+        static BaseTest()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        public BaseTest()
+        { 
+            
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Offlogs.Client.Tests/Clients/OfflogsHttpClientTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using OffLogs.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Offlogs.Client.Tests.Clients
+{
+    public class OfflogsHttpClientTests: BaseTest
+    {
+        private readonly OfflogsHttpClient Client;
+
+        public OfflogsHttpClientTests()
+        {
+            Client = new OfflogsHttpClient(ApiToken);
+        }
+
+        [Fact]
+        public async Task ShouldSendSimpleDebugLog()
+        {
+            await Client.SendLogAsync(LogLevel.Debug, "Some Debug message");
+        }
+
+        public async Task ShouldSendSimpleErrorLog()
+        {
+            await Client.SendLogAsync(LogLevel.Error, "Some Error message");
+        }
+
+        public async Task ShouldSendSimpleTraceLog()
+        {
+            await Client.SendLogAsync(LogLevel.Trace, "Some Trace message");
+        }
+
+        public async Task ShouldSendSimpleCriticalLog()
+        {
+            await Client.SendLogAsync(LogLevel.Critical, "Some Critical message");
+        }
+
+        public async Task ShouldSendSimpleWarningLog()
+        {
+            await Client.SendLogAsync(LogLevel.Warning, "Some Warning message");
+        }
+
+        public async Task ShouldSendSimpleInformationLog()
+        {
+            await Client.SendLogAsync(LogLevel.Information, "Some Information message");
+        }
+    }
+}
