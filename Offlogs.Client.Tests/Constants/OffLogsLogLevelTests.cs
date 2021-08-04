2e24aa8060c07f9dfa82c8e4036bf85a4a6b5bc9
Heorhii Shyra <hshy@isd.dp.ua>
2021-08-04 11:58:32 +0300

    #10 Added constant tests

---
+++ Offlogs.Client.Tests/Constants/OffLogsLogLevelTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using OffLogs.Client.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Offlogs.Client.Tests.Constants
+{
+    public class OffLogsLogLevelTests
+    {
+        [Theory]
+        [InlineData(LogLevel.Debug, "D")]
+        [InlineData(LogLevel.Trace, "D")]
+        [InlineData(LogLevel.Warning, "W")]
+        [InlineData(LogLevel.Critical, "F")]
+        [InlineData(LogLevel.Information, "I")]
+        [InlineData(LogLevel.Error, "E")]
+        [InlineData(LogLevel.None, "D")]
+        public void ShouldReturnOfflogLogTypeName(LogLevel logLevel, string offLogLogName)
+        {
+            Assert.Equal(
+                OffLogsLogLevel.GetFromLogLevel(logLevel).GetValue(),
+                offLogLogName
+            );
+        }
+    }
+}
